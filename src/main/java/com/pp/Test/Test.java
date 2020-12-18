f7d6605cecc6adb70e262d9dd2d0ec13da305588
JH <1712026004@qq.com>
2020-12-18 11:22:53 +0800

    再次添加了一条输出语句1

--- src/main/java/com/pp/Test/Test.java
+++ src/main/java/com/pp/Test/Test.java
@@ -8,6 +8,7 @@ public class Test {
         System.out.println("1.0尝试bug");
         System.out.println("12/18");
         System.out.println("12/18手敲");
-        System.out.println("12/18提交");
+        System.out.println("12/18提交1");
+
     }
 }
